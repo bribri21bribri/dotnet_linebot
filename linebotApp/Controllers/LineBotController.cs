22042b9efaed38da946904890a586ebac855f9ea
bribri21bribri <kuroiwaruiko21@gmail.com>
2024-07-09 17:24:41 +0800

    init project

---
+++ linebotApp/Controllers/LineBotController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace linebotApp.Controllers
+{
+    [ApiController]
+    [Route("api/linebot")]
+    public class LineBotController : Controller
+    {
+        private readonly string channelAccessToken = "";
+        private readonly string channelSecret = "";
+
+        [HttpPost("Webhook")]
+        public IActionResult Webhook()
+        {
+            return Ok();
+        }
+    }
+}
